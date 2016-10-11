1ab2ea7f4577cbd88d5c16a3face7ceaf2a6ff05
Doug DiFilippo <ddifilippo@wespeke.com>
2016-10-11 18:18:09 -0400

    Use a new tab for UI
    This still looks pretty awful but I just want it to be functional
    for now. I am going to write this in Elm or React or something
    more high level but I wanted a working version for now.

---
+++ src/background.js
@@ -0,0 +1,21 @@
+const extUrl = `chrome-extension://${location.host}/src/index.html`;
+
+const findExtTabOrNull = allTabs => allTabs.find(x => x.url === extUrl);
+
+const getAllTabs = () => new Promise(resolve => chrome.tabs.query({}, resolve));
+
+const logError = err => console.error('Error setting up extension tab', err);
+
+const openOrFocusTab = () => {
+    getAllTabs()
+        .then(findExtTabOrNull)
+        .then(tab => {
+            if(!tab) {
+                return chrome.tabs.create({ url: extUrl });
+            }
+
+            chrome.tabs.update(tab.id, { selected: true });
+        }).catch(logError);
+};
+
+chrome.browserAction.onClicked.addListener(openOrFocusTab);
--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
+const elErrorMessage = document.querySelector('.error-message');
+const elErrorMessageText = document.querySelector('.error-message-text');
+const elErrorMessageClose = document.querySelector('.error-message-close');
 const elBookmarks = document.querySelector('.bookmarks');
+const elJsonPasteWrapper = document.querySelector('.json-paste-wrapper');
 const jsonTextArea = document.querySelector('.json-textarea');
 const submitJson = document.querySelector('.submit-json');
 
@@ -21,6 +25,11 @@ const render = items => {
         .join('<br/>');
 };
 
+const showError = msg => {
+    elErrorMessageText.innerHTML = msg;
+    elErrorMessage.classList.add('is-shown');
+};
+
 const insertLink = (parentId, { title, url }) => {
     return new Promise(resolve => chrome.bookmarks.create({ parentId, title, url }, resolve));
 };
@@ -39,11 +48,22 @@ const insertLinks = (parentId, links) => {
         .then(() => insertLinks(parentId, tail));
 };
 
+const parseJsonPromise = json => {
+    return new Promise((resolve, reject) => {
+        try {
+            resolve(JSON.parse(json));
+        } catch (err) {
+            reject(`Uh oh, something couldn't parse in your JSON:<br/>${err}`);
+        }
+    });
+};
+
 const insertLinksFromJson = json => {
-    const data = JSON.parse(json);
     const currentParent = elBookmarks.dataset.currentId;
-    insertLinks(currentParent, data)
-        .then(() => chrome.bookmarks.getSubTree(currentParent, render));
+    return parseJsonPromise(json)
+        .then(data => insertLinks(currentParent, data))
+        .then(() => chrome.bookmarks.getSubTree(currentParent, render))
+        .catch(err => showError(err));
 };
 
 submitJson.addEventListener('click', function() {
@@ -56,7 +76,16 @@ submitJson.addEventListener('click', function() {
 
 elBookmarks.addEventListener('click', evt => {
     const id = evt.target.dataset.id;
+    if(!id) {
+        return;
+    }
+
+    elJsonPasteWrapper.classList.toggle('is-shown', id > 0);
     chrome.bookmarks.getSubTree(id, render);
+});
+
+elErrorMessageClose.addEventListener('click', evt => {
+    elErrorMessage.classList.remove('is-shown');
 });
 
 chrome.bookmarks.getTree(render);
