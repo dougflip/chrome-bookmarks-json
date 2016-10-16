76e6da3169b0e076aacab60a5cb56422e01a0f64
Doug DiFilippo <ddifilippo@wespeke.com>
2016-10-16 13:57:47 -0400

    Pull bookmarks via ports

--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,71 @@
-import Html exposing (div, text)
+port module Main exposing (..)
+
+import Html exposing (..)
+import Html.App as App
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import String
+
 
 main =
-    div [] [text "Hello world...again!"]
+  App.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+
+-- MODEL
+
+type alias Model =
+  { word : String
+  , suggestions : List String
+  }
+
+init : (Model, Cmd Msg)
+init =
+  (Model "" [], Cmd.none)
+
+
+-- UPDATE
+
+type Msg
+  = Change String
+  | Check
+  | Suggest (List String)
+
+
+port check : String -> Cmd msg
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    Change newWord ->
+      ( Model newWord [], Cmd.none )
+
+    Check ->
+      ( model, check model.word )
+
+    Suggest newSuggestions ->
+      ( Model model.word newSuggestions, Cmd.none )
+
+
+-- SUBSCRIPTIONS
+
+port suggestions : (List String -> msg) -> Sub msg
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  suggestions Suggest
+
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ input [ onInput Change ] []
+    , button [ onClick Check ] [ text "Check" ]
+    , div [] [ text (String.join ", " model.suggestions) ]
+    ]
--- src/elm-embed.js
+++ src/elm-embed.js
@@ -1,2 +1,18 @@
 var node = document.getElementById('main');
 var app = Elm.Main.embed(node);
+
+const getRootBookmarks = () => {
+    return new Promise((resolve, reject) => {
+        chrome.bookmarks.getTree(resolve);
+    }).then(xs => xs[0].children);
+}
+
+getRootBookmarks()
+    .then(xs => xs.map(x => x.title))
+    .then(app.ports.suggestions.send);
+
+app.ports.check.subscribe(word => {
+    getRootBookmarks()
+        .then(xs => xs.map(x => x.title))
+        .then(app.ports.suggestions.send);
+});
